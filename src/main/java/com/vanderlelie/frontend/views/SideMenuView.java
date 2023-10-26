da15647b2f06c0482ca1a817e95fc0f3849da0d0
Luke <lukeweerheim@hotmail.com>
2023-10-26 16:26:59 +0200

    feat: user context menu toggling

--- src/main/java/com/vanderlelie/frontend/views/SideMenuView.java
+++ src/main/java/com/vanderlelie/frontend/views/SideMenuView.java
@@ -4,11 +4,15 @@ import com.vanderlelie.frontend.controllers.AuthController;
 import com.vanderlelie.frontend.models.User;
 import com.vanderlelie.frontend.observers.AuthObserver;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 public class SideMenuView implements AuthObserver {
     @FXML
     private Label usernameLabel;
+    @FXML
+    private VBox userContextMenu;
     private AuthController authController;
 
     public void initialize() {
@@ -22,6 +26,12 @@ public class SideMenuView implements AuthObserver {
         } else {
             usernameLabel.setText("No Username");
         }
+
+        userContextMenu.setVisible(false);
+    }
+
+    public void toggleUserMenu() {
+        userContextMenu.setVisible(!userContextMenu.isVisible());
     }
 
     @Override
